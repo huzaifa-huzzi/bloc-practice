13385b03fea9f7e163f7df23b4123a40a003d19c
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-07-11 15:21:41 +0500

    1.Learning Bloc

--- lib/Ui/Counter_example.dart
+++ lib/Ui/Counter_example.dart
@@ -11,6 +11,30 @@ class CounterExample extends StatefulWidget {
 class _CounterExampleState extends State<CounterExample> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter Example'),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+         const  Center(
+            child: Text('0',style: TextStyle(fontSize: 25),),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: (){}, child:const  Text('Increment')),
+             const  SizedBox(width: 20,),
+              ElevatedButton(onPressed: (){}, child:const  Text('Decrement')),
+            ],
+          )
+
+        ],
+      ),
+    );
   }
 }
