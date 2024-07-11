ed1806667cf969707908a40977a5d991dba3cf6f
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-07-11 15:22:16 +0500

    1.Learning Bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_practice/Equatable/Equatable%20testing.dart';
+import 'package:bloc_practice/Ui/Counter_example.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const  MaterialApp(
-      home: EquatableTesting(),
+      home: CounterExample(),
     );
   }
 }
