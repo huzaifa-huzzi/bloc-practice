101523f17de3581b25d1305bcde2a8f06b70aec9
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-07-11 15:15:47 +0500

    1.Learning Bloc

---
+++ lib/Ui/Counter_example.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CounterExample extends StatefulWidget {
+  const CounterExample({super.key});
+
+  @override
+  State<CounterExample> createState() => _CounterExampleState();
+}
+
+class _CounterExampleState extends State<CounterExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
