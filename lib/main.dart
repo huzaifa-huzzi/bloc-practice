24102eca397a55bf6243283549d50ba0adec4b5e
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-07-16 15:44:17 +0500

    1.Learning Bloc

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:bloc_practice/Bloc/Posts%20Api/posts_bloc.dart';
 import 'package:bloc_practice/Bloc/Switch/switch_bloc.dart';
 import 'package:bloc_practice/Bloc/Todo/todo_bloc.dart';
 import 'package:bloc_practice/Repository/favourite_repositor.dart';
-import 'package:bloc_practice/Ui/Favourite/FavouriteScreen.dart';
 import 'package:bloc_practice/Ui/Post%20screen/PostScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
